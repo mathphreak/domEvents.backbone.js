85dc4f37ebdef7f93db1ee18303cd85dbebddd05
mathphreak <mathphreak@gmail.com>
2011-07-29 16:31:14 -0600

    added documentation hopefully

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
-/* Author: 
-
-*/
-
+var targetElement = $("#target-element").get(0);
+use(targetElement);
+targetElement.bind("mouse", function(event) {
+	alert(typeof event); // should be jQuery.Event
+	alert(event.type);
+});
 
 
 
