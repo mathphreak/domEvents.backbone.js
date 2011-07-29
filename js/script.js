a4c179150565c4e83f10317db5b6e2b79f845e30
mathphreak <mathphreak@gmail.com>
2011-07-29 16:59:25 -0600

    fixed fixing the demo

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ $(function() {
 	use(window);
 	window.bind("key", function(event) {
 		// event is a first-class jQuery.Event, with all the requisite properties.
-		alert(event.type);
+		$("#event-types-list").append(event.type + "<br />");
 	});
 });
 
